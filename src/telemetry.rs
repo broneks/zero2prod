afa60d360cd5e6b03b081012108b50089852b2ec
Bronek Szulc <bronek@fastmail.fm>
2024-05-16 11:35:49 -0400

    pg 117

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,16 +2,21 @@ use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::fmt::MakeWriter;
 
-pub fn get_subscriber(
+pub fn get_subscriber<Sink>(
     name: String,
     env_filter: String,
-) -> impl Subscriber + Send + Sync {
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+    where
+        Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new(env_filter));
     let formatting_layer = BunyanFormattingLayer::new(
         name,
-        std::io::stdout
+        sink
     );
     Registry::default()
         .with(env_filter)
