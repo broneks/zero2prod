432e0539de8bccc056ea16cddb821a7783a90fc1
Bronek Szulc <bronek@fastmail.fm>
2024-05-17 08:47:46 -0400

    pg 135

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,10 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
+    let connection_pool = PgPool::connect_lazy(
         &configuration.database.connection_string().expose_secret()
     )
-        .await
-        .expect("Failed to connect to Postgres.");
+        .expect("Failed to create Postgres connection pool.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
